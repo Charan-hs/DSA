67af15047a91655338571846f86d09b6b560e559
Charan <charan.hulivana@implevista.com>
2021-11-18 08:56:20 +0530

    binary Search loop and recursion

--- Python/Search/binarySearch.py
+++ Python/Search/binarySearch.py
@@ -1,3 +1,5 @@
+# Binary Search in loop
+
 def binarySearch(arr , l , r,x):
     while l<r:
         mid = (l+(r-1))//2
@@ -7,6 +9,20 @@ def binarySearch(arr , l , r,x):
             r= mid -1
         else : 
             l= mid+1
+    
+#Binary Search => Recursive
+
+def binarySearchRes(arr  ,l , r, x):
+    if l<r:
+        mid = (l+(r-1))//2
+        if(arr[mid] == x):
+            return x
+        elif arr[mid] > x:
+           return binarySearchRes(arr, l, mid-1,x)
+        else:
+           return binarySearchRes(arr , mid+1 , r, x)
+    else:
+        return
 
             
 def printResult(result , x):
@@ -17,7 +33,12 @@ def printResult(result , x):
     
 if(__name__ == "__main__"):
     arr = [x for x in range(1000)]
-    x= -564
+    x= 564
     printResult(binarySearch(arr ,0,len(arr), x),x)
+    printResult(binarySearchRes(arr ,0,len(arr), x),x)
     x=100000
     printResult(binarySearch(arr ,0,len(arr), x),x)
+    printResult(binarySearchRes(arr ,0,len(arr), x),x)
+
+
+
